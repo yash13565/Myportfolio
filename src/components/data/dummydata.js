20178888065363722016ccd84e4ed1808677bfd4
Yashwardhan Jaiswal <jaiswalyashwardhan20@gmail.com>
2023-04-12 09:57:48 +0530

    first commit

---
+++ src/components/data/dummydata.js
@@ -0,0 +1,164 @@
+import { Settings, CropRotate, ViewInAr, PieChart, Code, BarChart, CloudOutlined, FavoriteBorder, AddLocationAltOutlined, PhoneIphone, EmailOutlined, Facebook, Twitter, Instagram, YouTube } from "@mui/icons-material"
+
+export const navlink = [
+  {
+    url: "/",
+    text: "Home",
+  },
+  {
+    url: "/about",
+    text: "About",
+  },
+  {
+    url: "/services",
+    text: "Services",
+  },
+  {
+    url: "/portfolio",
+    text: "Portfolio",
+  },
+  {
+    url: "/contact",
+    text: "Contact",
+  },
+]
+export const home = [
+  {
+    text: "HELLO I'M",
+    name: "Yashwardhan Jaiswal",
+    post: "Frontend Developer",
+    design: "Web DESIGNER",
+    desc: "I love to create fully scalable and user-interactive websites.",
+  },
+]
+export const about = [
+  {
+    desc: "Hii,my name is Yashwardhan Jaiswal i am from Bilaspur Chhattisgarh.I have done my graduation in B.C.A from science College Bilaspur Chhatisgarh.My hobbies are playing outdoor games and also love to read short inspirational stories",
+    desc1: "A passionate frontend developer looking forward for an opportuinit to work in service as well as product based companies and help them to develop their products.",
+    cover: "./images/man.png",
+  },
+]
+export const services = [
+  {
+    id: 1,
+    icon: <Settings />,
+    title: "Creative Design",
+    desc: "I will provide unique design that will meet the user requirements.",
+  },
+  {
+    id: 2,
+    icon: <CropRotate />,
+    title: "Clean Code",
+    desc: "Trained in frontend technologies and i can write clean and effiecient code.",
+  },
+  {
+    id: 3,
+    icon: <ViewInAr />,
+    title: "Responsive Design",
+    desc: "Love to create responsive design for every screen adaptable device.",
+  },
+  {
+    id: 4,
+    icon: <PieChart />,
+    title: "Material UI	",
+    desc: "I know and i have used some of the famous react library like material ui in some of my projects",
+  },
+  {
+    id: 5,
+    icon: <Code />,
+    title: "Material UI Icons",
+    desc: "I know and i have used some of the famous react library like material ui icons in some of my projects",
+  },
+  {
+    id: 6,
+    icon: <BarChart />,
+    title: "Awesome Support",
+    desc: "I can provide better functionality and support to the customer with timely deliveries of the project.",
+  },
+]
+export const project = [
+  
+  {
+    id: 1,
+    icon: <FavoriteBorder />,
+    num: "6",
+    title: "PROJECTS COMPLETED",
+  }
+  
+]
+export const portfolio = [
+  {
+    id: 1,
+    cover: "../images/port/port1.jpg",
+    name: "Registration",
+    category: "Frontend website",
+    title: "Registration Form",
+  },
+  {
+    id: 2,
+    cover: "../images/port/port2.jpg",
+    name: "Gym",
+    category: "Frontend Website",
+    title: "Gym Website",
+  },
+  {
+    id: 3,
+    cover: "../images/port/port3.jpg",
+    name: "Twitter",
+    category: "Frontend Website",
+    title: "Twitter Clone",
+  },
+  {
+    id: 4,
+    cover: "../images/port/port4.jpg",
+    name: "Flipkart",
+    category: "E-commerce Website",
+    title: "Flikart Clone",
+  },
+  {
+    id: 5,
+    cover: "../images/port/port5.jpg",
+    name: "Docs",
+    category: "website DocEditor",
+    title: "Google Docs Clone",
+  },
+  {
+    id: 6,
+    cover: "../images/port/port6.jpg",
+    name: "Portfolio",
+    category: "Frontend Website",
+    title: "Yashwardhan Portfolio",
+  },
+]
+
+export const contact = [
+  {
+    icon: <AddLocationAltOutlined />,
+    text1: "Uslapur,Anand Nagar Behind White House",
+    text2: "Bilaspur Chhattisgarh",
+  },
+  {
+    icon: <PhoneIphone />,
+    text1: "+91-7223931546",
+    text2: "+91-0345627891",
+  },
+  {
+    icon: <EmailOutlined />,
+    text1: "jaiswalyashwardhan20@gmail.com",
+    text2: "funkyheart20@gmail.com",
+  },
+]
+export const social = [
+  {
+    icon: <Facebook />,
+  },
+  {
+    icon: <Twitter />,
+  },
+  {
+    icon: <Instagram />,
+  },
+  {
+    icon: <YouTube />,
+  },
+]
---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Footer from "../common/Footer"
+import { Header } from "../common/Header"
+import { Home } from "../home/Home"
+import { About } from "./About"
+import { Contact } from "./Contact"
+import { Portfolio } from "./Portfolio"
+import { Services } from "./Services"
+export const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/services' component={Services} />
+          <Route exact path='/portfolio' component={Portfolio} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
+}
